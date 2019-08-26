b73d58d4e638a6729e5f32e74a4950ff9271081f
Dávid Halász <dhalasz@redhat.com>
2019-08-26 21:14:36 +0200

    Call the methods directly on MiqAeDomainDecorator

--- app/decorators/miq_ae_domain_decorator.rb
+++ app/decorators/miq_ae_domain_decorator.rb
@@ -4,14 +4,14 @@ class MiqAeDomainDecorator < MiqDecorator
   end
 
   def fonticon
-    @object.name == MiqAeDatastore::MANAGEIQ_DOMAIN ? 'ff ff-manageiq' : super
+    name == MiqAeDatastore::MANAGEIQ_DOMAIN ? 'ff ff-manageiq' : super
   end
 
   def fileicon
     if git_enabled?
       'svg/ae_git_domain.svg'
-    elsif object.top_level_namespace
-      "svg/vendor-#{@object.top_level_namespace.downcase}.svg"
+    elsif top_level_namespace
+      "svg/vendor-#{top_level_namespace.downcase}.svg"
     end
   end
 end
