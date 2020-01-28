4e030f3a2cf49604109a799c4066c6b417d8d204
Hilda Stastna <hstastna@redhat.com>
2020-01-28 18:55:19 +0100

    Add gtl_button_cell to ServiceTemplateDecorator for valid/invalid Service
    Issue: https://github.com/ManageIQ/manageiq-ui-classic/issues/6320

--- app/decorators/service_template_decorator.rb
+++ app/decorators/service_template_decorator.rb
@@ -14,4 +14,16 @@ class ServiceTemplateDecorator < MiqDecorator
   def quadicon
     fileicon ? {:fileicon => fileicon} : {:fonticon => fonticon}
   end
+
+  def gtl_button_cell
+    v = template_valid?
+    {
+      :is_button => true,
+      :text      => _("Order"),
+      :title     => t = v ? _("Order this Service") : _("This Service cannot be ordered"),
+      :alt       => t,
+      :disabled  => !v,
+      :onclick   => "miqOrderService(\"#{id}\");"
+    }
+  end
 end
